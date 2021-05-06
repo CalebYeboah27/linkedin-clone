801206ab226f57848c1367f61159adbd1eec6cf0
calebyeboah27 <calebyeboah27@gmail.com>
2021-05-06 16:43:41 +0000

    Navigation Icons added

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <Container>
+      <h2>Main</h2>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  grid-area: main;
+`;
+
+export default Main;
